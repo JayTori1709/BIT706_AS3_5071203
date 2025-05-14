940fd889288fa9684c622f3f078233bad6feacdb
JayTori1709 <jaydenleviwood@outlook.co.nz>
2025-05-14 21:28:49 +1200

    Part D: Added XML comments

--- NationalMicrochipRegistry/Registry.Data/Services/AnimalService.cs
+++ NationalMicrochipRegistry/Registry.Data/Services/AnimalService.cs
@@ -3,21 +3,37 @@ using Registry.Data.Models;
 
 namespace Registry.Data.Services
 {
+    /// <summary>
+    /// Provides services related to animal management.
+    /// </summary>
     public class AnimalService
     {
         private readonly RegistryDbContext _context;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AnimalService"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
         public AnimalService(RegistryDbContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Adds a new animal to the database.
+        /// </summary>
+        /// <param name="animal">The animal to add.</param>
         public void AddAnimal(Animal animal)
         {
             _context.Animals.Add(animal);
             _context.SaveChanges();
         }
 
+        /// <summary>
+        /// Finds an animal assigned to a specific microchip.
+        /// </summary>
+        /// <param name="microchipId">The ID of the microchip.</param>
+        /// <returns>The assigned animal, or null if none found.</returns>
         public Animal FindByChip(int microchipId)
         {
             return _context.Microchips
